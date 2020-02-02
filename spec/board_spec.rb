56726cce1da5000f7fb7957a7ec3b5d2fc11e7e5
root <hish.b92@gmail.com>
2020-02-02 21:46:13 +0000

    refactor

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -2,19 +2,19 @@ require 'board'
 
   describe Board do
 
-    it {is_expected.to respond_to :table}
-    
-    it 'raises an error if outside of board contsraint' do
-      board = Board.new
-      ship = Ship.new("A1", 3, "N")
-      expect{subject.place ship}.to raise_error 'Out of bounds'
+    it { is_expected.to respond_to :table }
+   
+    describe '#place' do
+    let(:out_of_bounds_ship) { Ship.new("A1", 3, "N") } # need to use a Double
+    let(:ship) { Ship.new("A1", 1, "N") }
+
+    it 'raises an error if outside of board constraint' do
+      expect { subject.place out_of_bounds_ship }.to raise_error 'Out of bounds'
     end
-    
+
     it 'raises an error if ship overlaps' do
-      board = Board.new
-      ship1 = Ship.new("A1",1,"N")
-      board.place ship1
-      ship2 = Ship.new("A1",1,"N")
-      expect{board.place ship2}.to raise_error 'occupied'
+      subject.place ship
+      expect { subject.place ship }.to raise_error 'occupied'
     end
   end
+end
