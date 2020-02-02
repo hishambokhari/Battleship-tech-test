30af18a1a7095aa86fdec39f7ed7cbef393367b1
root <hish.b92@gmail.com>
2020-02-02 21:41:08 +0000

    Failing test

--- lib/board.rb
+++ lib/board.rb
@@ -1,11 +1,34 @@
+require_relative 'ship'
+
 class Board
   require 'terminal-table'
+  attr_accessor :boats
+  
+  def initialize
+    @boats = []
+  end
 
-
-  def size
-    size = Terminal::Table.new do |t|
-      t << ['A1']
+  def table
+    table = Terminal::Table.new do |t|
+    t << ['A1']
     end
   end
 
+  def place(ship)
+    if ship.size > 1
+      fail 'Out of bounds'
+    elsif overlap(ship) == true
+      fail 'occupied'
+    else
+      boats << ship
+    end
+end
+
+  def overlap(ship)
+    if @boats.map{|ship| ship.postion}.include?(ship.position)
+      return true
+    else
+      return false
+    end
+  end
 end
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -1,7 +1,20 @@
 require 'board'
 
-describe Board do
+  describe Board do
 
-  it {is_expected.to respond_to :size}
-
-end
+    it {is_expected.to respond_to :table}
+    
+    it 'raises an error if outside of board contsraint' do
+      board = Board.new
+      ship = Ship.new("A1", 3, "N")
+      expect{subject.place ship}.to raise_error 'Out of bounds'
+    end
+    
+    it 'raises an error if ship overlaps' do
+      board = Board.new
+      ship1 = Ship.new("A1",1,"N")
+      board.place ship1
+      ship2 = Ship.new("A1",1,"N")
+      expect{board.place ship2}.to raise_error 'occupied'
+    end
+  end
